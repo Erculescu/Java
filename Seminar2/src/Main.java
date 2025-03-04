99d218d4753dd2b0b2914adc21b05e7a8a86b668
Erculescu Rares <erculescur@gmail.com>
2025-03-04 13:06:13 +0200

    Java

---
+++ Seminar2/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        var nota=new Nota("Java",9);
+        System.out.println(nota);
+nota.setNota(10);
+        System.out.println(nota);
+        var ion=new Student(1,"Ion","1018",anul.II);
+        ion.add_nota(nota);
+        ion.add_nota(new Nota("PAW",8));
+        ion.add_nota(new Nota("Java",8));
+        System.out.println(ion);
+
+    }
+}
